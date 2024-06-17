ea68a9c59bb2e984fd35b25dce848d4852084b4f
bada308 <203190@jnu.ac.kr>
2024-06-17 23:29:06 +0900

    feat: redirect 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/board/list',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
