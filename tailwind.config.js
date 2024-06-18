15cf9ebc4ff788185711139e6290570c453fee86
bada308 <203190@jnu.ac.kr>
2024-06-18 22:21:21 +0900

    feat: tailwind colors theme 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                black: '#171717',
+                darkGray: '#666666',
+                gray: '#8D8D8D',
+                lightGray: '#D6D6D6',
+                white: '#f5f5f5',
+                background: '#FAFAFA',
+                blue: '#0070F3',
+                error: '#EE0000',
+            },
+            fontFamily: {
+                LineSeedKR: ['LineSeedKR', 'sans-serif'],
+                Pretendard: ['Pretendard', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
