b5e4291b7ab372d66a44a43a3b5f9b640bd3ed09
bada308 <203190@jnu.ac.kr>
2024-06-18 22:30:35 +0900

    feat: Header 컴포넌트 구현

---
+++ components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = ({ children }: React.HTMLAttributes<HTMLDivElement>) => {
+    return <div className="w-full h-20 border-b border-lightGray">{children}</div>;
+};
+
+export default Header;
