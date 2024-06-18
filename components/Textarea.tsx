e097227ad6f0255100a4b9159182253ad82642fa
bada308 <203190@jnu.ac.kr>
2024-06-18 23:51:28 +0900

    feat: Textarea 컴포넌트 구현

---
+++ components/Textarea.tsx
@@ -0,0 +1,18 @@
+interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+    text: string;
+    setText: (text: string) => void;
+    placeholder?: string;
+}
+
+const Textarea = ({ text, setText, placeholder }: TextareaProps) => {
+    return (
+        <textarea
+            className="resize-none w-full outline-none font-Pretendard h-full px-3 py-6 placeholder:text-darkGray"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder={placeholder ?? 'Type something here...'}
+        />
+    );
+};
+
+export default Textarea;
