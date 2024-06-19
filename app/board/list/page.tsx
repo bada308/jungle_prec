9e6b25e36124a7d6ae4c0b5aa6c1545728439cbc
bada308 <203190@jnu.ac.kr>
2024-06-19 18:28:47 +0900

    feat: 게시글 리스트 페이지와 상세 페이지 연결

--- app/board/list/page.tsx
+++ app/board/list/page.tsx
@@ -1,10 +1,6 @@
-import Button from '@/components/Button';
-import Header from '@/components/Header';
-import UserInfo from '@/components/UserInfo';
 import { getBoardList } from '@/features/board';
 import { BoardInfoDto } from '@/features/board/apis/dtos';
 import BoardList from '@/features/board/components/list/BoardList';
-import Link from 'next/link';
 import React from 'react';
 
 const BoardListPage = async () => {
--- features/board/components/list/BoardListItem.tsx
+++ features/board/components/list/BoardListItem.tsx
@@ -1,19 +1,24 @@
 import Title from '@/components/Title';
+import Link from 'next/link';
 
 interface BoardListItemProps {
+    boardId: string;
     title: string;
     content: string;
     createdAt: string;
 }
 
-const BoardListItem = ({ title, content, createdAt }: BoardListItemProps) => {
+const BoardListItem = ({ boardId, title, content, createdAt }: BoardListItemProps) => {
     content = content.length > 100 ? content.slice(0, 100) + '...' : content;
     return (
-        <div className="flex flex-col gap-4 py-9 border-b border-lightGray">
-            <Title>{title}</Title>
-            <p className="font-Pretendard">{content}</p>
-            <p className="text-gray text-sm">{createdAt}</p>
-        </div>
+        <Link href={`/board/detail/${boardId}`}>
+            {' '}
+            <div className="flex flex-col gap-4 py-9 border-b border-lightGray cursor-pointer">
+                <Title>{title}</Title>
+                <p className="font-Pretendard">{content}</p>
+                <p className="text-gray text-sm">{createdAt}</p>
+            </div>
+        </Link>
     );
 };
 
