3f69758b257adcc1f958b8e343b04e6c209bffbd
bada308 <203190@jnu.ac.kr>
2024-06-19 21:16:51 +0900

    feat: UnderlineContainer 디자인 수정

--- components/UnderlineContainer.tsx
+++ components/UnderlineContainer.tsx
@@ -1,7 +1,7 @@
 interface UnderlineContainerProps extends React.PropsWithChildren<{}> {}
 
 const UnderlineContainer = ({ children }: UnderlineContainerProps) => {
-    return <div className="border-b-[1.5px] border-gray w-full h-fit">{children}</div>;
+    return <div className="border-b-2 border-lightGray w-full h-fit">{children}</div>;
 };
 
 export default UnderlineContainer;
