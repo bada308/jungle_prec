c06b71200363cb670dc58db9bdf5e50afc7a8985
bada308 <203190@jnu.ac.kr>
2024-06-19 16:33:05 +0900

    feat: BorderedInput의 label을 optional props로 변경

--- components/BorderedInput.tsx
+++ components/BorderedInput.tsx
@@ -2,7 +2,7 @@ import Input from './Input';
 
 interface BorderedInputProps {
     id: string;
-    label: string;
+    label?: string;
     value: string;
     setValue: (value: string) => void;
     placeholder?: string;
@@ -12,9 +12,11 @@ interface BorderedInputProps {
 const BorderedInput = ({ id, label, value, setValue, placeholder, children }: BorderedInputProps) => {
     return (
         <div className="flex flex-col gap-2 w-full">
-            <label htmlFor={id} className="text-gray">
-                {label}
-            </label>
+            {label && (
+                <label htmlFor={id} className="text-gray">
+                    {label}
+                </label>
+            )}
             <div className="w-full flex gap-1 h-fit items-center">
                 <div className="border-2 border-gray rounded-lg px-4 py-3 w-full">
                     <Input id={id} value={value} setValue={setValue} placeholder={placeholder} />
