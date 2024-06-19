fcfa101ebacbf53b93e5ebddfdb1fde3037c08e1
bada308 <203190@jnu.ac.kr>
2024-06-19 13:26:36 +0900

    feat: Header 스타일 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,11 @@
 interface HeaderProps extends React.PropsWithChildren<{}> {}
 
 const Header = ({ children }: HeaderProps) => {
-    return <div className="w-full h-20 border-b border-lightGray">{children}</div>;
+    return (
+        <div className="w-full h-20 border-b border-lightGray flex justify-between items-center px-10 fixed top-0 bg-white">
+            {children}
+        </div>
+    );
 };
 
 export default Header;
