3496436a95826d7b8bb8f7a8d9e8d4c5c6280e9c
bada308 <203190@jnu.ac.kr>
2024-06-20 01:16:32 +0900

    feat: 로그인 및 회원가입 폼 구현

--- features/auth/components/login/LoginForm.tsx
+++ features/auth/components/login/LoginForm.tsx
@@ -2,35 +2,45 @@
 
 import BorderedInput from '@/components/BorderedInput';
 import Button from '@/components/Button';
-import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { postLogin } from '../../apis/login';
+
+interface LoginData {
+    userId: string;
+    password: string;
+}
 
 const LoginForm = () => {
-    const [userId, setUserId] = useState('');
-    const [password, setPassword] = useState('');
+    const { register, handleSubmit } = useForm<LoginData>();
+    const onSubmit: SubmitHandler<LoginData> = (data) => {
+        postLogin(data).then((res) => {
+            console.log(res);
+            if (res.msg === '로그인 성공') {
+                alert('로그인 성공');
+            } else {
+                alert('로그인 실패');
+            }
+        });
+    };
+
     return (
-        <div className="flex flex-col gap-14">
+        <form className="flex flex-col gap-14" onSubmit={handleSubmit(onSubmit)}>
             <h1 className="text-4xl font-bold w-full text-center">Login</h1>
             <div className="w-96 space-y-6">
+                <BorderedInput name="userId" placeholder="아이디 입력" label="아이디" register={register} required />
                 <BorderedInput
-                    id="login-id"
-                    value={userId}
-                    setValue={(v) => setUserId(v)}
-                    placeholder="아이디 입력"
-                    label="아이디"
-                />
-                <BorderedInput
-                    id="login-password"
-                    value={password}
-                    setValue={(v) => setPassword(v)}
+                    name="password"
                     placeholder="비밀번호 입력"
                     type="password"
                     label="비밀번호"
+                    register={register}
+                    required
                 />
             </div>
             <Button color="black" size="lg">
                 로그인
             </Button>
-        </div>
+        </form>
     );
 };
 
--- features/auth/components/signup/SignupForm.tsx
+++ features/auth/components/signup/SignupForm.tsx
@@ -2,60 +2,56 @@
 
 import BorderedInput from '@/components/BorderedInput';
 import Button from '@/components/Button';
-import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { postSignup } from '../../apis/signup';
+
+interface SignupData {
+    userId: string;
+    password: string;
+    passwordCheck: string;
+}
 
 const SignupForm = () => {
-    const [name, setName] = useState('');
-    const [userId, setUserId] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordCheck, setPasswordCheck] = useState('');
+    const { register, handleSubmit } = useForm<SignupData>();
+    const onSubmit: SubmitHandler<SignupData> = (data) => {
+        postSignup({ userId: data.userId, password: data.password }).then((res) => {
+            if (res.msg === '회원가입이 완료되었습니다.') {
+                alert('회원가입 성공');
+            } else {
+                alert('회원가입 실패');
+            }
+        });
+    };
     return (
-        <div className="flex flex-col gap-14">
+        <form className="flex flex-col gap-14" onSubmit={handleSubmit(onSubmit)}>
             <h1 className="text-4xl font-bold w-full text-center">Sign Up</h1>
             <div className="w-96 space-y-6">
-                <BorderedInput
-                    id="username"
-                    value={name}
-                    setValue={(v) => setName(v)}
-                    placeholder="이름 입력"
-                    label="이름"
-                >
+                <BorderedInput name="userId" placeholder="아이디 입력" label="아이디" register={register} required>
                     <Button color="black" size="md">
                         중복확인
                     </Button>
                 </BorderedInput>
                 <BorderedInput
-                    id="userId"
-                    value={userId}
-                    setValue={(v) => setUserId(v)}
-                    placeholder="아이디 입력"
-                    label="아이디"
-                >
-                    <Button color="black" size="md">
-                        중복확인
-                    </Button>
-                </BorderedInput>
-                <BorderedInput
-                    id="password"
-                    value={password}
-                    setValue={(v) => setPassword(v)}
+                    name="password"
                     placeholder="비밀번호 입력"
                     type="password"
                     label="비밀번호"
+                    register={register}
+                    required
                 />
                 <BorderedInput
-                    id="password-check"
-                    value={passwordCheck}
-                    setValue={(v) => setPasswordCheck(v)}
+                    name="passwordCheck"
                     placeholder="비밀번호 다시 입력"
                     type="password"
                     label="비밀번호 확인"
+                    register={register}
+                    required
                 />
             </div>
             <Button color="black" size="lg">
                 회원가입
             </Button>
-        </div>
+        </form>
     );
 };
 
