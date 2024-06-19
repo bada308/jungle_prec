bed9d228f35de0c922d0d898d4c7b1bc398255ca
bada308 <203190@jnu.ac.kr>
2024-06-19 13:54:58 +0900

    feat: 게시글 리스트 페이지에 Link 추가

--- app/board/list/page.tsx
+++ app/board/list/page.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header';
 import { getBoardList } from '@/features/board';
 import { BoardInfoDto } from '@/features/board/apis/dtos';
 import BoardList from '@/features/board/components/list/BoardList';
+import Link from 'next/link';
 import React from 'react';
 
 const BoardListPage = async () => {
@@ -22,14 +23,16 @@ const BoardListPage = async () => {
 
 const HeaderContent = () => {
     return (
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-4 items-center">
             <span className="text-darkGray">
                 <span className="text-blue">닉네임</span>님 반가워요!
             </span>
             <div>
-                <Button color="white" size="md">
-                    새 글 작성
-                </Button>
+                <Link href="/board/create">
+                    <Button color="white" size="md">
+                        새 글 작성
+                    </Button>
+                </Link>
             </div>
         </div>
     );
